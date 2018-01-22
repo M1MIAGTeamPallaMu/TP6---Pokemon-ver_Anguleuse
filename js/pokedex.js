0d25bc4c3e8944b193c42a3961725932754641c3
Unknown <hugo.palla@gmail.com>
2018-01-22 12:21:39 +0100

    Searching pokemons from the pokemon

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,8 +1,23 @@
 var pokeApp = angular.module('pokedex', ['ngResource']);
 
 // With this you can inject POKEAPI url wherever you want
-pokeApp.constant('POKEAPI', 'http://pokeapi.co');
+pokeApp.constant('POKEAPI', 'https://pokeapi.co');
 
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
+
+pokeApp.controller('pokeList', function($scope, $resource) {
+    $scope.iChooseYou = function(choice) {
+        console.log(choice);
+    };
+
+    var ApiData = $resource("https://pokeapi.co/api/v1/pokedex/1/");
+    var pokedex = ApiData.get().$promise.then(function(results){
+        $scope.pokemons = results.pokemon;
+    });
+
+});
+
+pokeApp.controller('pokeChoice', function($scope) {
+});
